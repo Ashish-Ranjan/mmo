2a32e17737deae28bf24c33be24ff668793967b5
NSSX <anthony.vella.2409@gmail.com>
2017-05-23 01:30:25 +0200

    ll

--- server.js
+++ server.js
@@ -5,4 +5,14 @@ var server = http.createServer(function(req, res) {
   res.end('Salut tout le monde !');
   console.log("connected");
 });
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+    console.log('Un client est connecté !');
+});
+
+
 server.listen(8080);
